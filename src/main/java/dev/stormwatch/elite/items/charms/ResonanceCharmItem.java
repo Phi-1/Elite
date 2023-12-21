9426c2a9c664a746395669da4282bbdf553686bc
Phi <dterberg98@gmail.com>
2023-12-22 00:20:32 +0100

    resonance gives gloweing

--- src/main/java/dev/stormwatch/elite/items/charms/ResonanceCharmItem.java
+++ src/main/java/dev/stormwatch/elite/items/charms/ResonanceCharmItem.java
@@ -8,6 +8,9 @@ import dev.stormwatch.elite.registry.EliteEntityTypes;
 import dev.stormwatch.elite.util.TickTimers;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -30,6 +33,11 @@ public class ResonanceCharmItem extends CharmItem implements CooldownAbilityItem
         CooldownMarker cooldownMarker = stack.getCapability(CooldownMarkerProvider.CAPABILITY_TYPE).orElseThrow(() -> new IllegalStateException("Resonance does not have a cooldown marker"));
         if (cooldownMarker.isOnCooldown(getCooldownMillis())) return; // TODO: cooldown feedback
         player.level().playSound(null, player.blockPosition(), SoundEvents.WARDEN_SONIC_CHARGE, SoundSource.PLAYERS, 0.8f, 1.2f);
+
+        int delay = 50;
+        for (LivingEntity living : player.level().getEntitiesOfClass(LivingEntity.class, player.getBoundingBox().inflate(64))) {
+            living.addEffect(new MobEffectInstance(MobEffects.GLOWING, delay + 20));
+        }
         TickTimers.schedule(() -> {
             ResonantBoom boom = EliteEntityTypes.RESONANT_BOOM.get().create(player.level());
             if (boom == null) return;
@@ -40,7 +48,7 @@ public class ResonanceCharmItem extends CharmItem implements CooldownAbilityItem
             player.level().playSound(null, player.blockPosition(), SoundEvents.WARDEN_SONIC_BOOM, SoundSource.PLAYERS, 1.0f, 1.2f);
             player.setDeltaMovement(player.getDeltaMovement().add(boom.getDeltaMovement().scale(-0.4)));
             player.hurtMarked = true;
-        }, 40);
+        }, delay);
         cooldownMarker.putOnCooldown();
         player.getCooldowns().addCooldown(this, COOLDOWN_TICKS);
     }
